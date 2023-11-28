f2baa91d8dd2578fe3677ea4c68268a090c3639a
Manaspal12 <152255490+Manaspal12@users.noreply.github.com>
2023-11-28 07:30:27 +0000

    javascript basic done

---
+++ 01-basics/test.js
@@ -0,0 +1,8 @@
+console.log("hello world")
+/*javascript is a high level scripting language used to perform the task on fronted as well as backend.
+javascript use both clientside as well as server side.
+javascript is most used programming language for web development.
+in javascript we use "ES6" version is the lastest one development in 2015.
+*/
+*ES6=Echma script6(vanilla javascript)
+javascript is basiclly used for designing the logic part of our web pages.
